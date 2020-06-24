990181ea0b704ff4b4067e684ef36125a94c433e
ProfesseurPoire <guyot.ced@gmail.com>
2020-06-24 16:50:04 +0200

    f

--- src/corgi/containers/tests/unit/FilledTree.cpp
+++ src/corgi/containers/tests/unit/FilledTree.cpp
@@ -331,18 +331,18 @@ TEST_F(TestTree, tree_is_parent)
     assert_that(node31->is_parent(*node11), equals(false));
 }
 
-TEST_F(TestTree, tree_push_back_copy)
-{
-    bool result = node11->children().push_back(*node32);
+// TEST_F(TestTree, tree_push_back_copy)
+// {
+//     bool result = node11->children().push_back(*node32);
 
-    assert_that(result, equals(true));
+//     assert_that(result, equals(true));
 
-    assert_that(node11->children().size(), equals(3));
-    assert_that(node21->children().size(), equals(2));
+//     assert_that(node11->children().size(), equals(3));
+//     assert_that(node21->children().size(), equals(2));
 
-    assert_that(node11->children()[2].get().number, equals(32));
-    assert_that(node11->children()[2].children()[0].get().number,equals(41));
-}
+//     assert_that(node11->children()[2].get().number, equals(32));
+//     assert_that(node11->children()[2].children()[0].get().number,equals(41));
+// }
 
 // TEST_F(TestTree, tree_push_back_move)
 // {
